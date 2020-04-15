6351829aa96983f6f2a48f46d2631c8885e4d44f
Jae-Son Song <jaesonsong@gmail.com>
2020-04-15 14:59:44 -0400

    Add User::find_by_name

--- W5D3/aa_questions/users.rb
+++ W5D3/aa_questions/users.rb
@@ -21,6 +21,18 @@ class User
         User.new(data.first)
     end
 
+    def self.find_by_name(fname, lname)
+        data = QuestionsDatabase.instance.execute(<<-SQL, fname, lname)
+            SELECT
+                *
+            FROM
+                users
+            WHERE
+                fname = ? AND lname = ?
+        SQL
+        User.new(data.first)
+    end
+
     def initialize(options)
         @id = options['id']
         @fname = options['fname']
